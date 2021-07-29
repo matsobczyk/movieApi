8ebcaa5181dc77f83b97364d0832eb98996e55a9
Mateusz Sobczyk <sobczyk.m@outlook.com>
2021-07-29 14:16:23 +0200

    fixed code formatting

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,22 +2,21 @@ const { loginValidation } = require("../middlewares/validation");
 const axios = require('axios')
 const jwt = require('jsonwebtoken')
 
-//login user
 exports.login = (async (req, res) => {
-
-	const {error} = loginValidation(req.body);
+	const { error } = loginValidation(req.body);
 	if (error) return res.status(400).send(error.details[0].message);
-
+	
 	const user = await axios({
 		method: 'post',
 		url: 'http://localhost:8081/auth',
 		data: {
-		  username: req.body.username,
-		  password: req.body.password,
+			username: req.body.username,
+			password: req.body.password,
 		}
-	  }).catch((error) => {
-        console.log(error.message);
-    });
+	}).catch((error) => {
+		console.log(error.message);
+	});
+
 	try {
 		res.status(200).json(user.data)
 	} catch (error) {
