122d71e38f78f34409c97b75901e1f767870d3a7
Mateusz Sobczyk <sobczyk.m@outlook.com>
2021-07-29 15:08:44 +0200

    using headers authorization

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 
 exports.auth = (req, res, next) => {
-    const token = req.header('auth-token');
+    const token = req.headers.authorization.split(" ")[1];
     if(!token) return res.status(400).send('Acces Denied!');
     try{
         const verified = jwt.verify(token, process.env.JWT_SECRET);
